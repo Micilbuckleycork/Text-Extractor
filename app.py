d976d89fd3c0dd453505702b8b7c282fe87eeefd
H Suhas Prabhu <85097320+Suhas-2002@users.noreply.github.com>
2023-06-15 21:12:24 +0530

    Support for pdf and docx

--- app.py
+++ app.py
@@ -1,25 +1,48 @@
 import streamlit as st
 from PIL import Image
 import pytesseract
+import tempfile
+import os
+import pdf2image
+import docx2txt
 
-# Set up Streamlit app title and description
-st.title("Image Text Extractor")
-st.write("Upload an image and extract text from it.")
+st.title("Text Extractor")
+st.write("Upload an image, PDF, or Word document and extract text from it.")
 
-# Display file uploader
-uploaded_file = st.file_uploader("Choose an image", type=["png", "jpg", "jpeg"])
+uploaded_file = st.file_uploader("Choose a file", type=["png", "jpg", "jpeg", "pdf", "docx"])
 
-# Check if a file has been uploaded
 if uploaded_file is not None:
-    # Load the image
-    img = Image.open(uploaded_file)
-    
-    # Display the uploaded image
-    st.image(img, caption="Uploaded Image", use_column_width=True)
-    
-    # Perform OCR and extract text from the image
-    extracted_text = pytesseract.image_to_string(img)
-    
-    # Display the extracted text
-    st.header("Extracted Text")
-    st.code(extracted_text)
+    file_extension = uploaded_file.name.split(".")[-1].lower()
+
+    if file_extension in ["png", "jpg", "jpeg"]:
+        img = Image.open(uploaded_file)
+        st.image(img, caption="Uploaded Image", use_column_width=True)
+        extracted_text = pytesseract.image_to_string(img)
+        st.header("Extracted Text")
+        st.code(extracted_text)
+
+    elif file_extension == "pdf":
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_filename = temp_file.name
+            temp_file.write(uploaded_file.getbuffer())
+
+        images = pdf2image.convert_from_path(temp_filename)
+
+        extracted_text = ""
+        for image in images:
+            st.image(image, caption="Extracted Image", use_column_width=True)
+            img_gray = image.convert("L")
+            text = pytesseract.image_to_string(img_gray)
+            extracted_text += text + "\n"
+        st.header("Extracted Text")
+        st.code(extracted_text)
+        os.remove(temp_filename)
+
+    elif file_extension == "docx":
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_filename = temp_file.name
+            temp_file.write(uploaded_file.getbuffer())
+        extracted_text = docx2txt.process(temp_filename)
+        st.header("Extracted Text")
+        st.code(extracted_text)
+        os.remove(temp_filename)
